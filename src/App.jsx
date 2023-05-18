a52aefcc2eb4aee6ca205a85351ef780fd92a2c1
geetha <kothageetha2001@gmail.com>
2023-05-18 18:37:23 +0530

    evening

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import {Body} from "./homepage/Body"
-import {Navbar} from "./homepage/Navbar"
+// import {Body} from "./homepage/Body"
+//  import {Navbar} from "./homepage/Navbar"
+ import Signin from "./Signin/Signin"
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Body/>
-      
+       {/* <Navbar/>
+      <Body/> */}
+      <Signin/> 
+      <h1>heloo</h1>
     </div>
   );
 }
--- src/homepage/Navbar.jsx
+++ src/homepage/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import PaidIcon from '@mui/icons-material/Paid';
+ import PaidIcon from '@mui/icons-material/Paid';
 
 export const Navbar = () => {
   return (
     <>
      <div id="navbar">
-         <h1>BillSplitter<PaidIcon /></h1>
+         <h1>BillSplitter<PaidIcon/></h1> 
 
         <div id="nav-buttons">
         <button>Register</button>
